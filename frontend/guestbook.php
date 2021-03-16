b15998b7113038e7a94ce728a6fbe650cf162d3d
leohori <46529218+leohori@users.noreply.github.com>
2021-03-15 23:28:57 -0300

    Add files via upload

--- frontend/guestbook.php
+++ frontend/guestbook.php
@@ -5,19 +5,17 @@ ini_set('display_errors', 1);
 require 'Predis/Autoloader.php';
 Predis\Autoloader::register();
 if (isset($_GET['cmd']) === true) {
-  $host = 'redis-master';
+  $host = 'redis-master-srv';
   header('Content-Type: application/json');
   if ($_GET['cmd'] == 'set') {
     $client = new Predis\Client(['scheme' => 'tcp', 'host' => $host, 'port' => 6379,]);
     $client->set($_GET['key'], $_GET['value']);
     print('{"message": "Updated"}');
   } else {
-    $host = 'redis-slave';
+    $host = 'redis-slave-srv';
     $client = new Predis\Client(['scheme' => 'tcp', 'host' => $host, 'port' => 6379,]);
     $value = $client->get($_GET['key']);
     print('{"data": "' . $value . '"}');
   }
-} else {
-  phpinfo();
 }
 ?>
