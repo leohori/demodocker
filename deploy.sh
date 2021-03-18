56f5dd43c5a5f1ba09bc6487dd0f1e1a5bd8187e
leohori <46529218+leohori@users.noreply.github.com>
2021-03-18 14:03:19 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ kubectl create -f ./yaml/namespace-manifest.yaml
 kubectl create -f ./yaml/frontend/deployment-manifest.yaml
 kubectl create -f ./yaml/frontend/service-manifest.yaml
 
-kubectl create -f ./yaml/redis-master/deployment-manifest.yaml
-kubectl create -f ./yaml/redis-master/service-manifest.yaml
+kubectl create -f ./yaml/redis_master/deployment-manifest.yaml
+kubectl create -f ./yaml/redis_master/service-manifest.yaml
 
-kubectl create -f ./yaml/redis-slave/deployment-manifest.yaml
-kubectl create -f ./yaml/redis-slave/service-manifest.yaml
+kubectl create -f ./yaml/redis_slave/deployment-manifest.yaml
+kubectl create -f ./yaml/redis_slave/service-manifest.yaml
